b7ad5ccf18a0ea0679540fbcb97b877f3479dac8
ravisingh8v <singhravi4v@gmail.com>
2025-12-29 16:53:27 +0530

    added expense apis

--- obj/Debug/net9.0/ExpenseTracker.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExpenseTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9eb4b1461f9b8824f4b0016c7eec37f27b7c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
